54a748a4c8bb3e715037144f02af89fc0608587f
Miguel Aréjula Aísa <arejula10@gmail.com>
2025-10-27 08:31:43 +0100

    add astro/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://are-dev.es/',
+
   devToolbar: {
     enabled: false
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 })
